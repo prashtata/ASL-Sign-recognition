a0e9eedb8dc9257383aef175930b44844235c336
prashtata <prashtata@gmail.com>
2025-03-12 19:31:27 -0700

    Redundant files deleted

--- cuda_check.py
+++
@@ -1,6 +0,0 @@
-import torch
-print(f"CUDA Available: {torch.cuda.is_available()}")
-print(f"Number of GPUs: {torch.cuda.device_count()}")
-print(f"Current Device: {torch.cuda.current_device()}")
-if torch.cuda.is_available():
-    print(f"Device Name: {torch.cuda.get_device_name(0)}")
